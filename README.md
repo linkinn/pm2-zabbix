f493991f14ea596e3a73f67ec2ea9380a0170fed
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2020-12-17 10:36:39 -0300

    refactor: add return number for status

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
 # development
 
 ### Create file pm2-status.conf in /etc/zabbix/zabbix_agentd.conf.d
+
 $ sudo nano /etc/zabbix/zabbix_agentd.conf.d/pm2-status.conf
+
 ```
 UserParameter=pm2.discovery,/usr/local/bin/node /etc/zabbix/script/pm2-zabbix/pm2.js
 UserParameter=pm2.status[*],/usr/local/bin/node /etc/zabbix/script/pm2-zabbix/pm2.js $1
 ```
 
 ### Create folder scripts in /etc/zabbix
+
 $ sudo mkdir /etc/zabbix/script
 $ sudo chown zabbix.zabbix -R /etc/zabbix/scripts
 
 ### Download git pm2.js in /etc/zabbix/script
+
 $ cd /etc/zabbix/scripts
 $ git clone https://github.com/linkinn/pm2-zabbix.git
 $ cd pm2-zabbix
-$ npm install pm2 --save
+$ npm install
 
 ### Create folder zabbix in /var/lib
+
 $ sudo mkdir /var/lib/zabbix
 
 ### Alter passwd zabbix
+
 $ sudo /etc/passwd
+
 ```
 zabbix:x:105:115::/var/lib/zabbix:/bin/false
 ```
 
 ### Execute pm2 in bash zabbix
+
 $ sudo su -s /bin/bash zabbix
--- pm2.js
+++ pm2.js
@@ -1,33 +1,52 @@
-const pm2 = require('pm2');
+const pm2 = require("pm2")
 
 const args = process.argv.slice(2)
 
+function intStatus(status) {
+  if (status === "online") return 1
+  else if (status === "stopped") return 0
+  else if (status === "errored") return 2
+  else return 3
+}
+
 pm2.connect(function (err) {
-  if(err) {
-          console.log("Error", err.message)
-          process.exit(2)
+  if (err) {
+    console.log(3)
+    process.exit(2)
   }
+
   pm2.list((err, list) => {
-    if(err) {
-      console.log(err)
+    if (err) {
+      console.log(3)
       process.exit(2)
     }
+
     if (list.length === 0) {
+      console.log(3)
       process.exit(2)
     }
 
     if (args.length > 0) {
-      const pm2ServiceStatus = list.filter(l => {
+      const pm2ServiceStatus = list.filter((l) => {
         return l.name === args[0]
       })
-      console.log(pm2ServiceStatus[0].pm2_env.status)
-      process.exit(2)
-      return
+
+      try {
+        const status = intStatus(pm2ServiceStatus[0].pm2_env.status)
+        console.log(status)
+        process.exit(2)
+        return
+      } catch {
+        console.log(3)
+        return
+      }
     }
 
-    const pm2List = {data: list.map(l => {
-      return {"{#NAME}": l.name, "{#STATUS}": l.pm2_env.status}
-    })}
+    const pm2List = {
+      data: list.map((l) => {
+        return { "{#NAME}": l.name, "{#STATUS}": intStatus(l.pm2_env.status) }
+      })
+    }
 
     const pm2ListString = JSON.stringify(pm2List)
     console.log(pm2ListString)
